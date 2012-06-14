e1b4355047e390729c128a2131c37bf229cf2d28
Dan Zajdband <dan.zajdband@gmail.com>
2012-06-14 15:35:31 -0300

    rest test

--- test/rest.test.js
+++ test/rest.test.js
@@ -4,7 +4,8 @@
  */
 
 var Tuiter = require('../')
-  , keys = require('./keys.json');
+  , keys = require('./keys.json')
+  , methods = require('../lib/config.json').rest;
 
 var t;
 
@@ -15,6 +16,16 @@ describe('rest', function(){
     done();
   });
 
-  // To be done...
+
+  describe('#homeTimeline()', function(){
+    it('should return latest tweets from logged in user timeline', function(done){
+      t.homeTimeline({'include_rts' : false}, function(err, data){
+        if(err) throw err;
+        data.should.exists;
+        "object".should.equal(typeof data);
+        done();
+      });
+    });
+  });
 
 });
