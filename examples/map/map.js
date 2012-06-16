8d0c77f0cdd1755be8e92613130747172edb5ae8
Dan Zajdband <dan.zajdband@gmail.com>
2012-06-16 12:27:36 -0300

    added gmaps example

---
+++ examples/map/map.js
@@ -0,0 +1,76 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , Tuiter = require('../../')
+  , http = require('http')
+  , util = require('util')
+  , keys = require('./keys.json');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+/*
+ * Routes
+ */
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+var server = http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+var t = new Tuiter(keys);
+
+//world -180,-90,180,90
+//baires -65.6103515625,-40.44694705960048,-56.6015625,-33.28461996888768
+
+t.filter({locations: [{lat: -90, long: -180},{lat: 90, long: 180}]}, function(stream){
+
+  // New tweet
+  stream.on("tweet", function(data){
+    if(data.geo && data.geo.coordinates){
+      io.sockets.emit("tweet", {
+          coordinates: data.geo.coordinates
+        , screen_name: data.user.screen_name
+        , text: data.text
+        , pic: data.user.profile_image_url
+      });
+    }
+  });
+
+  stream.on("delete", function(data){
+    //I don't care about deleted tweets
+  });
+
+  // Log errors
+  stream.on("error", function(error){
+    // handle errors
+  });
+
+});
+
+process.on('uncaughtException', function(err){
+  util.inspect(err);
+});
---
+++ examples/map/public/js/map.js
@@ -0,0 +1,52 @@
+$(function(){
+
+  /*
+   * Render the gmap
+   */
+
+  var latlng = new google.maps.LatLng(0, 0);
+  var myOptions = {
+      zoom: 2
+    , center: latlng
+    , mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+    
+  var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+  /*
+   * socket.io
+   */
+
+  var socket = io.connect();
+  socket.on('tweet', function(data){
+
+    // Add marker
+
+    var myLatlng = new google.maps.LatLng(data.coordinates[0],data.coordinates[1]);
+
+    var marker = new google.maps.Marker({
+      position: myLatlng, 
+      animation: google.maps.Animation.DROP,
+      map: map
+    });  
+
+    // Remove marker after 30 seconds
+
+    setTimeout(function(){
+      marker.setMap(null);
+      delete marker;
+    }, 30000);
+
+    // Attach info window to marker
+
+    var infowindow = new google.maps.InfoWindow({
+      content: "<img src='"+data.pic+"' style='float:left; padding: 5px;' /><strong>"+data.screen_name+"</strong>: "+data.text
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+    });
+
+  });
+
+});
